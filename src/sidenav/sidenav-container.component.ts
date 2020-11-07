249af46f2e558faa625b0d1bd05a65d5d51e5c13
Philip Washington Sorst <philip@sorst.net>
2020-11-07 21:55:35 +0100

    Auto Closing Sidenav on route change

--- src/sidenav/sidenav-container.component.ts
+++ src/sidenav/sidenav-container.component.ts
@@ -1,15 +1,14 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import {MatSidenav} from '@angular/material/sidenav';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {MatDrawerMode, MatSidenav} from '@angular/material/sidenav';
 import {SidenavService} from './sidenav.service';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {BreakpointObserver} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 
 @Component({
     selector: 'ddr-mat-sidenav-container',
     templateUrl: './sidenav-container.component.html'
 })
-export class SidenavContainerComponent implements OnInit
+export class SidenavContainerComponent implements OnInit, OnChanges
 {
     @ViewChild('sidenav', {static: true})
     public sidenav: MatSidenav;
@@ -17,13 +16,9 @@ export class SidenavContainerComponent implements OnInit
     @Input()
     public stayOpenOnLargeScreen = false;
 
-    public screenLarge$: Observable<boolean>;
+    public mode$: Observable<MatDrawerMode>;
 
-    private largeBreakpoints = [
-        Breakpoints.Medium,
-        Breakpoints.Large,
-        Breakpoints.XLarge
-    ];
+    public opened$: Observable<boolean>;
 
     constructor(private sidenavService: SidenavService, private breakpointObserver: BreakpointObserver)
     {
@@ -32,18 +27,18 @@ export class SidenavContainerComponent implements OnInit
     /**
      * @override
      */
+    public ngOnChanges(changes: SimpleChanges): void
+    {
+        this.sidenavService.setStayOpenOnLargeScreen(changes.stayOpenOnLargeScreen.currentValue);
+    }
+
+    /**
+     * @override
+     */
     public ngOnInit(): void
     {
         this.sidenavService.setSidenav(this.sidenav);
-        this.screenLarge$ = this.breakpointObserver.observe(this.largeBreakpoints).pipe(
-            map(result => result.matches)
-        );
-    }
-
-    public closeSidebar(): void
-    {
-        if (!(this.stayOpenOnLargeScreen && this.breakpointObserver.isMatched(this.largeBreakpoints))) {
-            this.sidenav.close();
-        }
+        this.mode$ = this.sidenavService.getModeObservable();
+        this.opened$ = this.sidenavService.getOpenedObservable();
     }
 }
--- src/sidenav/sidenav.service.ts
+++ src/sidenav/sidenav.service.ts
@@ -1,5 +1,9 @@
 import {Injectable} from '@angular/core';
-import {MatDrawerToggleResult, MatSidenav} from '@angular/material/sidenav';
+import {MatDrawerMode, MatDrawerToggleResult, MatSidenav} from '@angular/material/sidenav';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {NavigationStart, Router} from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
@@ -8,9 +12,51 @@ export class SidenavService
 {
     private sidenav: MatSidenav;
 
+    private stayOpenOnLargeScreen = false;
+
+    private largeBreakpoints = [
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge
+    ];
+
+    private screenLarge$: Observable<boolean>;
+
+    private mode$: Observable<MatDrawerMode>;
+
+    private opened$: Observable<boolean>;
+
+    constructor(private breakpointObserver: BreakpointObserver, private router: Router)
+    {
+        this.screenLarge$ = this.breakpointObserver.observe(this.largeBreakpoints).pipe(
+            map(result => result.matches)
+        );
+        this.mode$ = this.screenLarge$.pipe(
+            map(large => large && this.stayOpenOnLargeScreen ? 'side' : 'over')
+        );
+        this.opened$ = this.screenLarge$.pipe(
+            map(large => large && this.stayOpenOnLargeScreen)
+        );
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                this.closeSidebar();
+            }
+        });
+    }
+
     public setSidenav(sidenav: MatSidenav): void
     {
         this.sidenav = sidenav;
+    }
+
+    public getStayOpenOnLargeScreen(): boolean
+    {
+        return this.stayOpenOnLargeScreen;
+    }
+
+    public setStayOpenOnLargeScreen(value: boolean): void
+    {
+        this.stayOpenOnLargeScreen = value;
     }
 
     public toggle(): Promise<MatDrawerToggleResult>
@@ -19,6 +65,25 @@ export class SidenavService
             return Promise.reject('No MatSidenav found. Use setSidenav() of SidenavService');
         }
 
-        return this.sidenav.toggle();
+        if (!(this.stayOpenOnLargeScreen && this.breakpointObserver.isMatched(this.largeBreakpoints))) {
+            return this.sidenav.toggle();
+        }
+    }
+
+    public closeSidebar(): void
+    {
+        if (!(this.stayOpenOnLargeScreen && this.breakpointObserver.isMatched(this.largeBreakpoints))) {
+            this.sidenav.close();
+        }
+    }
+
+    public getModeObservable(): Observable<MatDrawerMode>
+    {
+        return this.mode$;
+    }
+
+    public getOpenedObservable(): Observable<boolean>
+    {
+        return this.opened$;
     }
 }
