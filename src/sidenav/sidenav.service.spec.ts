6cefd46de875e902c7a95b291f6badc68a8b319f
Philip Washington Sorst <philip@sorst.net>
2020-11-07 21:59:32 +0100

    Fixing mode style

--- src/sidenav/sidenav.service.spec.ts
+++
@@ -1,8 +0,0 @@
-import {SidenavService} from './sidenav.service';
-
-describe('SidenavService', () => {
-    it('can be constructed without dependencies', () => {
-        const sidenavService = new SidenavService();
-        expect(sidenavService).toBeDefined();
-    });
-});
