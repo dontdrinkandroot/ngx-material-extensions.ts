56f28d0ef6dd3eaa802d91fc1cd9ac46f710302a
Philip Washington Sorst <philip@sorst.net>
2024-06-01 15:46:29 +0200

    refactor: Update to Angular 18

--- src/toolbar/toolbar-fixed-top.directive.ts
+++ src/toolbar/toolbar-fixed-top.directive.ts
@@ -1,27 +1,32 @@
-import {Directive, ElementRef, Input, OnInit} from "@angular/core";
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 
 @Directive({
     selector: '[ddrMatToolbarFixedTop]'
 })
-export class ToolbarFixedTopDirective implements OnInit {
+export class ToolbarFixedTopDirective implements OnInit
+{
 
     @Input()
-    public ddrMatToolbarFixedTop?: string = undefined;
+    public ddrMatToolbarFixedTop: string[] | '' = '';
 
-    constructor(private el: ElementRef) {
+    constructor(private el: ElementRef)
+    {
     }
 
     /**
      * @override
      */
-    public ngOnInit() {
+    public ngOnInit()
+    {
         const container = document.createElement('div');
         container.classList.add('ddr-mat-toolbar-fixed-top-container');
         this.el.nativeElement.parentElement.insertBefore(container, this.el.nativeElement);
         container.appendChild(this.el.nativeElement);
 
-        if (this.ddrMatToolbarFixedTop != null && '' !== this.ddrMatToolbarFixedTop) {
-            container.classList.add(this.ddrMatToolbarFixedTop);
+        if ('' !== this.ddrMatToolbarFixedTop) {
+            this.ddrMatToolbarFixedTop.forEach((className: string) => {
+                container.classList.add(className);
+            });
         }
     }
 }
